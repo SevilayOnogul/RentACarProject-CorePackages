3d8493a7dbca2e87fe9cc780e7008352037c609a
SevilayOnogul <sevilayonogul34@hotmail.com>
2024-09-23 17:54:24 +0300

    First Commit

---
+++ Core.CrossCuttingConcerns/Serilog/Messsages/SerilogMessages.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Serilog.Messsages;
+
+public static class SerilogMessages
+{
+    public static string NullOptionMessage => "You have sent a blank value! Something went wrong.Please try again.";
+}
